0a6c35c2da4ec09a7a795e0c32aef65b9bf9f799
maxph2211 <maxph22112000@gmail.com>
2022-08-13 00:09:49 +0700

    add

---
+++ VASR/server/flask/server.py
@@ -0,0 +1,43 @@
+import random
+import os
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    # get audio file and save it
+    audio_file = request.files["file"]
+    file_name = str(random.randint(0, 100000))
+    audio_file.save(file_name)
+
+    # invoke keyword spotting service
+    kss = Keyword_Spotting_Service()
+
+    # make a prediction
+    predicted_keyword = kss.predict(file_name)
+
+    # remove the audio file
+    os.remove(file_name)
+
+    # send back the predicted keyword in json format
+    data = {"keyword": predicted_keyword}
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
