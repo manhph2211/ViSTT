6a163d8d06670fa4ff1e632844005d9c0f137251
Phạm Hùng Mạnh (VNCDLL-CTÐTKSAI-HVCTÐTKSAI) <manhph5@vingroup.net>
2022-08-29 11:43:32 +0700

    add

--- VASR/server/flask/server.py
+++ VASR/server/flask/server.py
@@ -6,24 +6,14 @@ app = Flask(__name__)
 
 @app.route("/predict", methods=["POST"])
 def predict():
-
-    # get audio file and save it
     audio_file = request.files["file"]
-    file_name = str(random.randint(0, 100000))
-    audio_file.save(file_name)
-
-    # invoke keyword spotting service
-    kss = Keyword_Spotting_Service()
-
-    # make a prediction
-    predicted_keyword = kss.predict(file_name)
-
-    # remove the audio file
-    os.remove(file_name)
-
-    # send back the predicted keyword in json format
+    predicted_keyword = process(audio_file)
     data = {"keyword": predicted_keyword}
     return jsonify(data)
+
+
+def process(audio_path):
+    return ""
 
 
 if __name__ == "__main__":
